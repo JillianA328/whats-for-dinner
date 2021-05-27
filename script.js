b344930a3670387fd7fb869525b0e795f1fbc519
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-26 20:40:37 -0400

    added API url and API key

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var eatIn = "#eat-in"
-
+var apiKey = "300a7d2cc57843cd8827b55f1a6ceab1"
 // var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients"
 
 // fetch(apiUrl);
 // greated get recipes function 
 var getRecipes = function() {
+    fetch('https://api.spoonacular.com/recipes/complexSearch?key${apiKey}');
+    
     console.log("function was called");
 };
 
