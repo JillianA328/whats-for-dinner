6d748b52a07340c583be6776993a4a3b020e77d3
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-26 20:15:49 -0400

    added getRecipes function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var eatIn = "#eat-in";
+var eatIn = "#eat-in"
+
+// var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients"
+
+// fetch(apiUrl);
+// greated get recipes function 
+var getRecipes = function() {
+    console.log("function was called");
+};
+
+getRecipes();
