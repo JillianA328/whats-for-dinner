ec92ae6381ac689e61a3f4c7626739a54c588bfd
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-27 19:28:42 -0400

    adding function to call upon API

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var eatIn = "#eat-in"
-var apiKey = "300a7d2cc57843cd8827b55f1a6ceab1"
-// var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients"
+var eatIn = "#eat-in";
+var apiKey = "300a7d2cc57843cd8827b55f1a6ceab1";
+var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
 
 // fetch(apiUrl);
 // greated get recipes function 
 var getRecipes = function() {
-    fetch(`https://api.spoonacular.com/recipes/findByIngredients?key=${apiKey}`);
+    // fetch(`https://api.spoonacular.com/recipes/findByIngredients?key=${apiKey}`);
+    fetch(apiURL).then(function(reponse) {
+
+        if (Response.ok) {
+            Response.json().then(function(data) {
+                displayIssues(data);
+            })
+        }
+    })
     
     console.log("function was called");
 };
