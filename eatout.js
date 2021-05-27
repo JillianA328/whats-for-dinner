20c43d7909b25a716ac79d8d908edcd1e7f82990
damatgre <gregdamato2@gmail.com>
2021-05-27 15:15:19 -0400

    zip code api

--- eatout.js
+++ eatout.js
@@ -2,19 +2,8 @@ var apiKey = "bb0cfd0345318ff737b874e211c6bcbb";
 var zipCode = "08049";
 
 
-
-function getApi() {
-    fetch(`https://api.documenu.com/v2/restaurant/4072702673999819?key=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-};
-
-getApi();
-
 function zipApi() {
-    fetch(`https://api.documenu.com/v2/restaurants/zip_code/${zipCode}size=50?key=${apiKey}`)
+    fetch(`https://api.documenu.com/v2/restaurants/zip_code/${zipCode}?key=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
