1266815803b97d35839140b28cd278e317496b89
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-27 20:00:50 -0400

    added input checkbox

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var eatIn = "#eat-in";
 var apiKey = "300a7d2cc57843cd8827b55f1a6ceab1";
-var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
+// var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
 
 // fetch(apiUrl);
 // greated get recipes function 
 var getRecipes = function() {
+    // define API URL
+    var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
     // fetch(`https://api.spoonacular.com/recipes/findByIngredients?key=${apiKey}`);
-    fetch(apiURL).then(function(reponse) {
+    // https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true.
+    var apiURL =`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=apples,+flour,+sugar&number=5`
+    fetch(apiURL).then(function(response) {
 
-        if (Response.ok) {
-            Response.json().then(function(data) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log("API",data);
                 displayIssues(data);
             })
         }
