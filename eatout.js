3914d3141bb2ee640423c416dcc195f6f8a0f119
damatgre <gregdamato2@gmail.com>
2021-05-28 16:05:59 -0400

    map data for cards

--- eatout.js
+++ eatout.js
@@ -1,13 +1,47 @@
 var apiKey = "bb0cfd0345318ff737b874e211c6bcbb";
-var zipCode = "08049";
+var zipCode; //undefined
+var restName = "";
+var websiteUrl = "";
+var phoneNumber = "";
+var priceRating = "";
+
+//console.log(zipCode);
+
 
 
 function zipApi() {
-    fetch(`https://api.documenu.com/v2/restaurants/zip_code/${zipCode}?key=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
+
+    $("#search-btn").on("click", function (event) {
+        event.preventDefault();
+        zipCode = $("#zip-code").val().trim(); //defined variable
+        console.log(zipCode);
+
+        fetch(`https://api.documenu.com/v2/restaurants/zip_code/${zipCode}?key=${apiKey}`)
+            .then(response => response.json())
+            .then(({ data }) => {
+                console.log(data);
+                data.map(function (restaurant) {
+                    console.log(restaurant.restaurant_name);
+                    console.log(restaurant.address.formatted);
+                });
+            })
+        $("#zip-code").val("");
+    });
+
 };
 
 zipApi();
+
+
+
+//would like to use geolocation 
+
+//create cards in using html/bulma to show info
+//name
+//phone number
+//address
+//website link if there
+//price rating if there
+//can we get distance away?
+
+//link cuisine drop down?
