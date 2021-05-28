6a6e036c1c634bcc67d9050e51cf6c78211055a8
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-27 20:29:35 -0400

    added get recipes function + started display recipes function

--- script.js
+++ script.js
@@ -4,23 +4,57 @@ var apiKey = "300a7d2cc57843cd8827b55f1a6ceab1";
 
 // fetch(apiUrl);
 // greated get recipes function 
-var getRecipes = function() {
+var getRecipes = function(event) {
+    event.preventDefault()
     // define API URL
+    var userInput = document.querySelectorAll(".ingredients");
+    console.log(userInput)
+    var search =""
+    for (let i = 0; i < userInput.length; i++ ){
+        if (userInput[i].checked){
+            console.log(userInput[i].name)
+            search += `,+${userInput[i].name}`
+        }
+    }
+    console.log(search)
+    
     var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
     // fetch(`https://api.spoonacular.com/recipes/findByIngredients?key=${apiKey}`);
     // https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true.
-    var apiURL =`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=apples,+flour,+sugar&number=5`
+    var apiURL =`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${search}&number=5`
+    console.log(apiURL)
     fetch(apiURL).then(function(response) {
 
         if (response.ok) {
             response.json().then(function(data) {
                 console.log("API",data);
-                displayIssues(data);
+                displayRecipes(data);
             })
         }
     })
+
     
     console.log("function was called");
 };
 
-getRecipes();
+document.getElementById("searchBtn").addEventListener("click",getRecipes)
+
+function displayRecipes(data) {
+    var htmlCode = "";
+    for (let i=0; i<data.length; i++) {
+        htmlCode += `
+        <div class="tile is-parent">
+        <article class="tile is-child notification is-info">
+          <p class="title">Middle tile</p>
+          <p class="subtitle">With an image</p>
+          <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/640x480.png">
+          </figure>
+        </article>
+      </div>
+        `
+    }
+}
+
+
+// getRecipes();
