24c39e52efc8724a63d9a10ad9371a10ba7fc506
damatgre <gregdamato2@gmail.com>
2021-05-29 12:53:28 -0400

    set up cards for restaurants

--- eatout.js
+++ eatout.js
@@ -22,23 +22,28 @@ function zipApi() {
             .then(({ data }) => {
                 //console.log(data);
                 data.map(function (restaurant) {
-                    //test data
+                    //tested data
                     // console.log(restaurant.restaurant_name);
                     // console.log(restaurant.address.formatted);
                     // console.log(restaurant.restaurant_phone);
                     // console.log(restaurant.restaurant_website);
 
                     restName = restaurant.restaurant_name;
-                    restWebsite = restaurant.address.formatted;
+                    restAddress = restaurant.address.formatted;
                     restPhone = restaurant.restaurant_phone;
                     restWebsite = restaurant.restaurant_website;
 
                     //console.log(restName);
 
-                    var card = ('<div>This worked!</div>');
-                    $(card).addClass("restaurantCard");
-                    //$(card).attr('id', 'restaurantInfo')
+                    var card = $('<div>').addClass('card').attr('id', 'restaurantInfo');
+                    var cardBody = $('<div>').addClass('card-content');
+                    var cardTitle = $('<p>').addClass('title').text(restName);
+                    var cardSub = $('<p>').addClass('subtitle').text(restAddress);
+                    var cardInfo = $('<p>').addClass('content').text(`Phone Number: ${restPhone}`);
+                    var cardUrl = $('<p>').addClass('content').html(`Website: <a href="${restWebsite}" target="_blank">${restWebsite}</a>`);
 
+                    cardBody.append(cardTitle, cardSub, cardInfo, cardUrl);
+                    card.append(cardBody);
                     $("#displayRest").append(card);
                 });
 
@@ -49,33 +54,6 @@ function zipApi() {
 };
 
 zipApi();
-
-// function zipApi() {
-
-//     $("#search-btn").on("click", function (event) {
-//         event.preventDefault();
-//         zipCode = $("#zip-code").val().trim(); //defined variable
-//         console.log(zipCode);
-
-//         fetch(`https://api.documenu.com/v2/restaurants/zip_code/${zipCode}?key=${apiKey}`)
-//             .then(response => response.json())
-//             .then(({ data }) => {
-//                     console.log(data);
-//                     console.log(data.restaurant_name);
-
-//                     for(i = 0; i < 5; i++){
-
-//                     }
-
-//             })
-//         $("#zip-code").val("");
-//     });
-
-// };
-
-// zipApi();
-
-
 
 
 //would like to use geolocation 
