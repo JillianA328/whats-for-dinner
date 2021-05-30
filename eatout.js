7391888abafb72343001817e63af08b54ef3f947
damatgre <gregdamato2@gmail.com>
2021-05-30 14:34:42 -0400

    styled title and subtitle

--- eatout.js
+++ eatout.js
@@ -9,24 +9,24 @@ var restPrice = "";
 //console.log(zipCode);
 
 
-
+//function to find zip and populate restaurant info
 function zipApi() {
 
     $("#search-btn").on("click", function (event) {
         event.preventDefault();
         zipCode = $("#zip-code").val().trim(); //defined variable
-        console.log(zipCode);
+        //console.log(zipCode);
 
         fetch(`https://api.documenu.com/v2/restaurants/zip_code/${zipCode}?key=${apiKey}`)
             .then(response => response.json())
             .then(({ data }) => {
-                //console.log(data);
+                console.log(data);
                 data.map(function (restaurant) {
                     //tested data
                     // console.log(restaurant.restaurant_name);
                     // console.log(restaurant.address.formatted);
                     // console.log(restaurant.restaurant_phone);
-                    // console.log(restaurant.restaurant_website);
+                    //console.log(restaurant.restaurant_website);
 
                     //variables to name data.map()
                     restName = restaurant.restaurant_name;
@@ -37,13 +37,13 @@ function zipApi() {
                     //console.log(restName);
 
                     //card elements
-                    var card = $('<div>').addClass('card column is-4 p-1 m-1').attr('id', 'restaurantInfo');
+                    var card = $('<div>').addClass('card column is-4 p-1 m-1 has-background-orange').attr('id', 'restaurantInfo');
                     var cardBody = $('<div>').addClass('card-content');
-                    var cardTitle = $('<p>').addClass('title').text(restName);
-                    var cardSub = $('<p>').addClass('subtitle').text(restAddress);
+                    var cardTitle = $('<p>').addClass('title is-size-1-mobile has-text-white').text(restName);
+                    var cardSub = $('<p>').addClass('subtitle is-size-3-mobile has-text-white').text(restAddress);
                     var cardFooter = $('<footer>').addClass('card-footer');
-                    var cardNum = $('<a>').addClass('card-footer-item').attr('href', `tel:${restPhone}`).text(`${restPhone}`);
-                    var cardUrl = $('<span>').addClass('card-footer-item').html(`Website <a href="${restWebsite}" target="_blank"></a>`);
+                    var cardNum = $('<a>').addClass('card-footer-item is-size-4-mobile').attr('href', `tel:${restPhone}`).text(`${restPhone}`);
+                    var cardUrl = $('<a>').addClass('card-footer-item is-size-4-mobile').attr('href', `https://${restName}.com`).html(`Website`);
 
                     //add info to card
 
@@ -55,14 +55,9 @@ function zipApi() {
 
             })
         $("#zip-code").val("");
+        //should I add same thing for displayRest to clear info?
     });
 
 };
 
 zipApi();
-
-function callRest(){
-    $('#phoneNumber').on("clicK", function(){
-        
-    })
-}
